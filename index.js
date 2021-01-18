e26e0f438db5967a10d386b238a91b3209d3a712
Jerome Velociter <j.velociter@agricool.co>
2021-01-18 21:00:39 +0100

    Demonstrate issue

---
+++ index.js
@@ -0,0 +1,18 @@
+const {read, write} = require('to-vfile');
+const remark = require('remark');
+const mdx = require('remark-mdx');
+
+(async () => {
+  const path = './example.mdx'
+  const file = await read(path)
+  const contents = await remark()
+    .use(mdx)
+    .use(() => tree => {
+      console.log(tree)
+    })
+    .process(file)
+  await write({
+    path,
+    contents
+  })
+})();
